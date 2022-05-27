7c6f830e6f9fc93311f1e86d5e46e7df168f5e7d
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-27 13:28:43 +0900

    3-2 dark 모드

--- src/apollo.js
+++ src/apollo.js
@@ -1,13 +1,24 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-const TOKEN = "token";
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE";
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
-export const darkModeVar = makeVar(false);
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
+
+export const enableDarkMode = () => {
+  localStorage.setItem(DARK_MODE, "enabled");
+  darkModeVar(true);
+};
+
+export const disableDarkMode = () => {
+  localStorage.removeItem(DARK_MODE);
+  darkModeVar(false);
+};
 
 export const logUserIn = (token) => {
   localStorage.setItem(TOKEN, token);
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -42,7 +42,6 @@ const LOGIN_MUTATION = gql`
 
 function Login() {
   const location = useLocation();
-
   const {
     register,
     handleSubmit,
